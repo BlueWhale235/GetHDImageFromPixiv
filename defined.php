aaf07ef7aa3793958d7e92280e07ebac5a84fc3f
bulewhale235 <coldwater235@foxmail.com>
2022-05-13 15:30:44 +0800

    .gitignore remove defined.php

---
+++ defined.php
@@ -0,0 +1,5 @@
+<?php
+define('_COOKIE', '');
+define('_REFERER', 'https://www.pixiv.net/');
+define('_LANG', 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7');
+define('_UA', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36 Edg/101.0.1210.39');
