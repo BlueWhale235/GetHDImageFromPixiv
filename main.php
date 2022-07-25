a9352625d0412e3e23a9e2c84a492d34fe98a25a
bulewhale235 <coldwater235@foxmail.com>
2022-07-25 20:06:15 +0800

    some text errors

--- main.php
+++ main.php
@@ -1,24 +1,8 @@
 <?php
-/*
-Copyright (C) 2022 BuleWhale
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
 require_once 'function.php';
 require_once 'defined.php';
 require_once 'vendor/autoload.php';
-/*  Init whoops and  */
+/*  Init whoops to debug  */
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
@@ -31,7 +15,7 @@ $headerHttp = [
     'accept-language'   => _LANG
 ];
 
-/*  MAIN
+/*  Main
 Variant:
 --  $arrFile    : array     --枚举目录文件
 --  $numArr     : int       --获取$arrFile成员数
